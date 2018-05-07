ab32fc70981fe596d3ae8072bd9408c6ddad49f7
Nicholas Hein <nichh81999@gmail.com>
2018-05-06 19:12:35 -0500

    Initial commit

---
+++ UnitTests/HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main ()
+{
+    cout << "Hello, world!" << endl;
+    string str = "";
+    cin >> str;
+    cout << "You wrote: " + str << endl;
+    return 0;
+}
